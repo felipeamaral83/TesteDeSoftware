c132acb0d5de3908028c6af19d4ec8c35db3db42
Felipe Amaral de Oliveira <felipe.oliveira-ext@viavarejo.com.br>
2021-04-09 17:18:36 -0300

    Pedido - Atualizar Item.

--- 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -32,6 +32,11 @@ namespace NerdStore.Vendas.Domain
             return _pedidoItens.Any(p => p.ProdutoId == pedidoItem.ProdutoId);
         }
 
+        private void ValidarPedidoItemInexistente(PedidoItem pedidoItem)
+        {
+            if (!PedidoItemExistente(pedidoItem)) throw new DomainException("O item não existe no pedido");
+        }
+
         private void ValidarQuantidadeItemPermitida(PedidoItem pedidoItem)
         {
             var quantidadeItens = pedidoItem.Quantidade;
@@ -58,6 +63,19 @@ namespace NerdStore.Vendas.Domain
             }
 
             _pedidoItens.Add(pedidoItem);
+            CalcularValorPedido();
+        }
+
+        public void AtualizarItem(PedidoItem pedidoItem)
+        {
+            ValidarPedidoItemInexistente(pedidoItem);
+            ValidarQuantidadeItemPermitida(pedidoItem);
+
+            var itemExistente = _pedidoItens.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId);
+
+            _pedidoItens.Remove(itemExistente);
+            _pedidoItens.Add(pedidoItem);
+
             CalcularValorPedido();
         }
 
